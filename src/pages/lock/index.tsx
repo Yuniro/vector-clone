334bf72a2a4da82993174a3493a74904dbb3f1cd
yuniro <sergey.r1130@gmail.com>
2022-04-30 04:37:28 +0300

    Updated lock page

--- src/pages/lock/index.tsx
+++ src/pages/lock/index.tsx
@@ -1,12 +1,28 @@
 import { FunctionComponent } from "react";
+import { Paper } from "@mui/material";
+import clsx from "clsx";
 import PageWrapper from "../../components/partials/page/wrapper";
+import InfoHeader from "../../components/lock/lockInfo";
+import LockTabs from "../../components/lock/lockTabs";
+import styles from "./styles.module.scss";
 
 interface Props {}
 
 const Lock: FunctionComponent<Props> = () => {
   return (
-    <PageWrapper title="Lock">
-      <div></div>
+    <PageWrapper title="Lock VTX">
+      <Paper
+        elevation={1}
+        className={clsx(styles.container, "overflow-hidden")}
+      >
+        <InfoHeader
+          increment={81}
+          lockedValue={0}
+          tvlValue={600755}
+          claimable={0}
+        />
+        <LockTabs />
+      </Paper>
     </PageWrapper>
   );
 };
